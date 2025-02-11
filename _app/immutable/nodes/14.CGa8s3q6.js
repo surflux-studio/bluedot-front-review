37c99f1221f0d8922c4fbf7fbc655ccfc9efae9c
surflux-studio <carl@surflux.studio>
2025-02-11 01:39:07 +0000

    Update from https://github.com/surflux-studio/bluedot-front/commit/8f386989de3fe7a195da0a8d4ba2041a334a30df

--- _app/immutable/nodes/14.CGa8s3q6.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.5W7zuegH.js";import{S as t,i as e}from"../chunks/index.6ch9Ev20.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
