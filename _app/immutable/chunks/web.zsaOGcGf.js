e4c7773fc84ed71e02467a387b630ac9af48a46c
surflux-studio <carl@surflux.studio>
2025-02-27 11:44:48 +0000

    Update from https://github.com/surflux-studio/bluedot-front/commit/b853825e93d9322bfd6aee9a685da35ad1ec65cf

---
+++ _app/immutable/chunks/web.zsaOGcGf.js
@@ -0,0 +1,1 @@
+import{W as n}from"./sign.D_3Z-9N_.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
--- _app/immutable/nodes/4.C6SNmxyp.js
+++
@@ -1,1 +0,0 @@
-import{s as o}from"../chunks/scheduler.CVGJxaOr.js";import{S as r,i as s}from"../chunks/index.1WceP6qL.js";import{g as a}from"../chunks/searchParams.DU-Kmf9q.js";function n(t){return a("/viewer",{},{replaceState:!0}),[]}class c extends r{constructor(e){super(),s(this,e,n,null,o,{})}}export{c as component};
