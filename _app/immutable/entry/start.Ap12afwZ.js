3dfbb528cf675bd6d7926d2cf1fbad430d51e714
surflux-studio <carl@surflux.studio>
2025-03-04 10:04:10 +0000

    Update from https://github.com/surflux-studio/bluedot-front/commit/67026cb6ea349e892d3f87d812589471d1006289

---
+++ _app/immutable/entry/start.Ap12afwZ.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CiRIqHzs.js";export{t as start};
---
+++ _app/immutable/nodes/4.BftLoPPq.js
@@ -0,0 +1,1 @@
+import{s as o}from"../chunks/scheduler.ChiloVl_.js";import{S as r,i as s}from"../chunks/index.CeuzkHdt.js";import{g as a}from"../chunks/searchParams.Ly2oIjVi.js";function n(t){return a("/viewer",{},{replaceState:!0}),[]}class c extends r{constructor(e){super(),s(this,e,n,null,o,{})}}export{c as component};
